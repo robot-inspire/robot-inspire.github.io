5f9ca8093fb5a12139838d731d81ba64c6d3fc7a
The Floating String <laurencel2001@gmail.com>
2023-05-29 21:51:55 -0400

    WIP add placeholder for contact team

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
+const profileImageStyle = {
+  borderRadius: '10%',
+}
+
 export default function Home() {
   return (
     <main className={styles.main}>
@@ -50,13 +54,44 @@ export default function Home() {
 
         <h3>Our Team</h3>
 
-        <p>Laurence Liang</p>
-	<p>Title</p>
-	<p>Description</p>
+        <div className={styles.landingTeamContainer}>
 
-        <p>Qi (Ken) Zuo</p>
-	<p>Title</p>
-	<p>Description</p>
+          <div className={styles.landingTeamItem}>
+
+            <Image
+              src="/Konstantin-Kleine-photo.jpg"
+              alt="Photo of mountains"
+              width="270"
+              height="196"
+              style={profileImageStyle}
+            />
+
+            <p><b>Laurence Liang</b></p>
+            <p>Technical Lead</p><br/>
+            <p>Laurence has written software in industry roles since 2018, from developing natural language processing tools at Expedia, to building the first prototype of the core API of a pre-seed startup with a seven-figure valuation. Laurence has also developed models for bioinformatics and machine learning research, and has presented his work at ISEF, CWSF and CUCAI. He has been featured on TEDx, is a recipient of the Cansbridge Fellowship during which Laurence developed visualization tools and predictive models for motor tests at ACSL Ltd. (Japan's largest industrial drone manufacturer), and is currently pursuing his degree in mechanical engineering at McGill University.</p>
+
+          </div>
+
+          <div className={styles.landingTeamItem}>
+
+            <Image
+                src="/Konstantin-Kleine-photo.jpg"
+                alt="Photo of mountains"
+                width="270"
+                height="196"
+                style={profileImageStyle}
+              />
+
+            <p><b>Qi (Ken) Zuo</b></p>
+            <p>Title</p>
+	    <br/>
+            <p>Description</p>
+
+          </div>
+
+        </div>
+
+
 
       </div>
 
